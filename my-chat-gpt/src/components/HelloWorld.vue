2bb817bad6c630dc057c9340368a31654571ae28
小卜 <11523050+xiao-bu39fh@user.noreply.gitee.com>
2023-02-16 21:42:53 +0800

    create img

--- my-chat-gpt/src/components/HelloWorld.vue
+++ my-chat-gpt/src/components/HelloWorld.vue
@@ -2,26 +2,45 @@
   <div class="container">
     <h2 class="header">
       ChatGpt
-
     </h2>
     <div ref="listWrapper" class="chat-list-wrapper">
-      <div class="chat-item" v-for="(chat, index) in chatList" :key="index">
+      <div class="chat-item"  v-for="(chat, index) in chatList" :key="index">
 
         <!-- 判断消息是从自己发出还是接收 -->
         <template v-if="chat.from === 'self'">
-          <p class="oneself">
+          <!-- showType === 1 对话模式 -->
+          <p class="oneself" v-if="chat.showType === 1 && chat.chatModel === 1">
+            <span>{{ chat.text }}</span>
+            <a href="javascript:;">
+              <img :src="chat.face" alt="">
+            </a>
+          </p>
+          <p class="oneself" v-else>
             <span>{{ chat.text }}</span>
             <a href="javascript:;">
               <img :src="chat.face" alt="">
             </a>
           </p>
         </template>
+
+        
         <template v-else>
-          <p class="AI">
+          <p v-if="chatModel === 1 && chat.showType === 1" class="AI">
             <a href="javascript:;">
               <img src="https://i.postimg.cc/jdS60KQW/openai.png" alt="">
             </a>
             <span>{{ chat.text }}</span>
+          </p>
+          <p v-if="chatModel === 0 && chat.showType === 0" class="AI">
+            <a v-if="chat.url || chat.text" href="javascript:;">
+              <img src="https://i.postimg.cc/jdS60KQW/openai.png" alt="">
+            </a>
+            <span v-if="chat.url" >              
+              <span v-for="item,i in chat.url" :key="i" href="">
+                <img @click="showImgFn(item.url)" :src="item.url" alt="" />
+              </span>
+            </span>
+            <span v-if="chat.text">{{chat.text}}</span>
           </p>
         </template>
 
@@ -30,12 +49,24 @@
     <div class="chat-wrapper">
       <input class="chatInput" type="text" v-model="prompt">
       <button :disabled="this.isAble" ref="sendBtn" @click="submitForm">{{ currentTime }}</button>
+      <button ref="changeModle" @click="changeModel">{{ chatModel === 1 ? "作图" : "对话" }}</button>
     </div>
     <div @click="close" :class="[isLoading ? 'loading' : 'close']" ref="loading"><span>努力响应中... 请稍后</span></div>
-    <div  :class="[isStart ? 'isStart' :'close']"> 
-      <input v-model="api_key" type="password"
-        placeholder="请输入API keys" />
+    <div :class="[isStart ? 'isStart' : 'close']">
+      <input v-model="api_key" type="password" placeholder="请输入API keys" />
+      <p>
+        temperature&nbsp;
+        <input class="range" type="range" ref="range" name="" id="" @change="changeRange" v-model="temperature" min="0"
+          max="1" step="0.1" />
+        <span>{{ temperature }}</span>
+      </p>
       <button @click="ok">确&nbsp;&nbsp;认</button>
+      <i>temperature：要使用的采样策略。 接近 1 的值会给模型带来更多风险/创造力，而接近 0 的值会生成明确定义的答案。</i>
+    </div>
+
+    <!--展示图片 -->
+    <div @click="closeShowImgFn" :class="{showImgUrl}">
+      <img @click="closeShowImgFn" :src="showImgUrl" alt="">
     </div>
   </div>
 </template>
@@ -50,7 +81,9 @@ export default {
   },
   data() {
     return {
-      isStart:false,
+      showImgUrl:'',
+      chatModel: 1,
+      isStart: false,
       currentTime: '发送',
       isAble: false,
       isLoading: false,
@@ -62,32 +95,73 @@ export default {
       model: 'text-davinci-003',
       mobile: true,
       response: '',
+      urlData:'',
       chatList: [
         {
           from: 'bot',
           face: 'https://i.postimg.cc/jdS60KQW/openai.png',
-          text: `嗨，很高兴认识你！一般来说，每分钟提问你不能超过两次。尽管可能有某些情况在短时间内可以接受多次提问，但一般情况下，推荐的最大问答频率是一分钟不超过两次。`
+          text: `嗨 靓仔，很高兴认识你！`,
+          showType:1
+        },
+        {
+          from: 'bot',
+          face:
+            avatar,
+            url:'',
+          text: '想画什么，详细描述给我',
+          showType:0
         },
         {
           from: 'self',
           face:
             avatar,
-          text: '嗨，很高兴认识你'
+          text: '嗨，很高兴认识你',
+          showType:1
         }
-      ]
+      ],
+      temperature: 0,
+      baseUrl: 'https://api.openai.com/v1/completions'
     };
   },
-  mounted(){
+  mounted() {
     this.isStart = true
   },
   methods: {
+    showImgFn(url) {
+      this.showImgUrl = url
+    },
+    closeShowImgFn() {
+      this.showImgUrl = ''
+    },
+    changeModel() {
+      this.chatModel = this.chatModel === 1 ? 0 : 1
+      this.baseUrl = this.chatModel === 1 ? 'https://api.openai.com/v1/completions' :'https://api.openai.com/v1/images/generations'
+      console.log(this.chatModel)
+    },
     submitForm() {
-      //函数防抖
-      let data = {
-        prompt: this.prompt,
-        model: this.model,
-        max_tokens: this.max_tokens,
+      if (this.api_key === '') {
+        alert('请传入ApiKey,然后进行操作')
+        return
       }
+      let data = null;
+      //函数防抖
+      if (this.chatModel === 1) {
+        data = {
+          prompt: this.prompt,
+          model: this.model,
+          max_tokens: this.max_tokens,
+        }
+      } else {
+        //作图模式
+        data = {
+          prompt: this.prompt,
+          n: 1,
+          size: "1024x1024"
+        }
+      }
+
+
+
       if (!this.isAble) {
         this.isLoading = true
       }
@@ -97,7 +171,7 @@ export default {
           avatar,
         text: this.prompt
       });
-      axios.post('https://api.openai.com/v1/completions', data, {
+      axios.post(this.baseUrl, data, {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ` + this.api_key,
@@ -105,19 +179,33 @@ export default {
       })
         .then(response => {
           this.isLoading = false
-          this.response = response.data.choices[0].text;
-          this.chatList.push({
-            from: 'bot',
-            face:
-              'https://i.postimg.cc/jdS60KQW/openai.png',
-            text: this.response
-          });
+          if (this.chatModel === 1) {//对话模式
+            this.response = response.data.choices[0].text;
+            this.chatList.push({
+              from: 'bot',
+              face:
+                'https://i.postimg.cc/jdS60KQW/openai.png',
+              text: this.response,
+              showType:1
+            });
+          }else if(this.chatModel === 0){//作图模式
+            this.response = response.data
+            this.chatList.push({
+              from: 'bot',
+              face:
+                'https://i.postimg.cc/jdS60KQW/openai.png',
+              url: this.response.data,
+              showType:0
+            });
+          }
+
           console.log(this.response)
 
         })
         .catch(error => {
           this.isLoading = true
           this.$refs.loading.children[0].innerText = error
+          console.log('error :', error)
           setTimeout(() => {
             this.isLoading = false
           }, 1000);
@@ -145,7 +233,7 @@ export default {
     close() {
       this.isLoading = false
     },
-    ok(){
+    ok() {
       this.isStart = false
     }
   },
@@ -166,7 +254,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-.loading,.isStart,
+.showImgUrl{
+  position: absolute;
+  z-index: 999;
+  width: 100vw;
+  height: 100vh;
+  background-color: #0000008a;
+  display: flex;
+  justify-content: center;
+  top:0;
+  align-items: center;
+  img{
+    width: 100vw;
+    height: auto;
+  }
+}
+.loading,
+.isStart,
 .close {
   position: absolute;
   width: 100vw;
@@ -183,31 +287,58 @@ export default {
   transition: all .2s linear;
   background-color: #00000044;
 
+  p {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    span {
+      width: 20px;
+    }
+
+    input {
+      width: 150px;
+      padding: 0;
+      transform: none;
+    }
+  }
+
+  i {
+    font-size: 14px;
+    margin-top: 90px;
+    transform: scale(0.8);
+  }
+
   span {
     display: block;
     width: 80%;
     text-align: center;
   }
 }
-.isStart{
+
+.isStart {
   display: flex;
   flex-direction: column;
-  background-color: #000000a6;
+  justify-content: flex-end;
+  background-color: #000000d7;
+
   input {
     border: none;
     padding: 2px 5px;
     display: block;
-    margin-left: 5px;
     height: 40px;
+    padding-left: 15px;
     transition: all 0s;
     width: 280px;
-    border-radius: 4px;
+    border-radius: 25px;
+    transform: scale(0.9);
   }
-  button{
+
+  button {
     border: none;
     width: 200px;
-    height: 50px;
-    color: rgb(5, 14, 19);
+    height: 30px;
+    color: #000;
     font-size: 18px;
     border-radius: 50px;
     background-color: #85FFBD;
@@ -216,9 +347,10 @@ export default {
     background-image: -moz-linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%);
     background-image: -o-linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%);
     background-image: linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%);
-      margin-top: 40px;
+    margin-top: 100px;
   }
 }
+
 .close {
   opacity: 0;
   z-index: -15;
@@ -252,16 +384,17 @@ export default {
     padding-bottom: 15px;
 
     .chat-item {
-      padding: 10px 5px;
+      padding: 0 8px;
+      p{
+        margin: 8px 0;
+      }
     }
 
     .oneself {
       display: flex;
-      padding: 5px;
       justify-content: flex-end;
       align-items: flex-start;
       flex-shrink: 1;
-      margin-top: 15px;
 
       span {
         max-width: 68vw;
@@ -282,13 +415,13 @@ export default {
         margin-left: 15px;
         border-radius: 50%;
         overflow: hidden;
-
         img {
           width: 100%;
           height: 100%;
         }
       }
     }
+
 
     .AI {
       display: flex;
@@ -311,8 +444,12 @@ export default {
         padding: 5px;
         text-align: justify;
         box-shadow: 0 0 18px #45454542;
+        overflow: hidden;
+        img{
+          width: 100%;
+          height: 100%;
+        }
       }
-
       a {
         width: 40px;
         height: 40px;
@@ -349,14 +486,14 @@ export default {
     }
 
     button {
-      width: 70px;
+      width: 58px;
+      transform: scale(0.9);
       border-radius: 8px;
       border: none;
+      font-weight: 900;
       height: 30px;
-      color: #fff;
-      background-color: #00DBDE;
-      background-image: linear-gradient(90deg, #00DBDE 0%, #FC00FF 100%);
-
+      color: #222222;
+      font-size: 14px;
     }
   }
 }
